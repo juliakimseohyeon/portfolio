3316f76e49a137e01d2beaa9d7641d8cb7bac6d7
Julia Kim <juliakimseohyeon@gmail.com>
2024-06-12 12:18:01 -0400

    Set up basic file markup of Project page

---
+++ src/assets/pages/Project/Project.jsx
@@ -0,0 +1,21 @@
+import "./Project.scss";
+
+export default function Project() {
+  return (
+    <main>
+      <section className="hero">
+        <div className="hero__text">
+          <h1>Hi, I'm Julia</h1>
+          <h2>Scroll down for the good stuff</h2>
+        </div>
+        <div className="hero__image"></div>
+      </section>
+      <section className="">
+        <h2>Skills</h2>
+      </section>
+      <section className="">
+        <h2>Projects</h2>
+      </section>
+    </main>
+  );
+}
