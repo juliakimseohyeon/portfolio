8163c28ed43dfd91dafc8c86737ae5c1fdbddc59
Julia Kim <juliakimseohyeon@gmail.com>
2024-06-19 15:03:30 -0400

    Add style to ProjectDetail page

--- src/pages/ProjectDetail/ProjectDetail.jsx
+++ src/pages/ProjectDetail/ProjectDetail.jsx
@@ -1,7 +1,9 @@
 import "./ProjectDetail.scss";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import axios from "axios";
+import iconGithub from "../../assets/icons/icon-github.svg";
+import iconUrl from "../../assets/icons/icon-url.svg";
 
 export default function ProjectDetail() {
   const [selectedProject, setSelectedProject] = useState({});
@@ -30,40 +32,49 @@ export default function ProjectDetail() {
     console.log("Selected project: ", selectedProject);
     return (
       <div className="container">
-        <div>
-          <img
-            src={`${import.meta.env.VITE_API_URL}${selectedProject.image}`}
-          />
-          <div className="project-hero">
+        <img
+          className="project-hero__image"
+          src={`${import.meta.env.VITE_API_URL}${selectedProject.image}`}
+        />
+        <div className="project-hero">
+          <div className="project-hero__text">
+            <h1 className="project-hero__title">
+              {selectedProject.project_name}
+            </h1>
             <div className="project-hero__links">
-              <h1>{selectedProject.project_name}</h1>
-              <p>{selectedProject.url}</p>
-              <p>{selectedProject.github_frontend}</p>
-            </div>
-            <div className="project-hero__tags">
-              {selectedProject.tags &&
-                selectedProject.tags.map((tag, idx) => <p key={idx}>tag</p>)}
+              <Link to={selectedProject.url}>
+                <img src={iconUrl} />
+              </Link>
+              <Link to={selectedProject.github_frontend}>
+                <img src={iconGithub} />
+              </Link>
             </div>
           </div>
-          <div className="project-description">
-            {selectedProject.description &&
-              Object.entries(selectedProject.description).map(
-                ([key, value]) => (
-                  <div key={key}>
-                    <h2>{key}</h2>
-                    {Array.isArray(value) ? (
-                      <ul>
-                        {value.map((item, idx) => (
-                          <li key={idx}>{item}</li>
-                        ))}
-                      </ul>
-                    ) : (
-                      <p>{value}</p>
-                    )}
-                  </div>
-                )
-              )}
+          <div className="project-hero__tag-group">
+            {selectedProject.tags &&
+              selectedProject.tags.map((tag, idx) => (
+                <p key={idx} project-hero__tag>
+                  {tag}
+                </p>
+              ))}
           </div>
+        </div>
+        <div className="project-description-group">
+          {selectedProject.description &&
+            Object.entries(selectedProject.description).map(([key, value]) => (
+              <div key={key} className="project-description">
+                <h2>{key}</h2>
+                {Array.isArray(value) ? (
+                  <ul>
+                    {value.map((item, idx) => (
+                      <li key={idx}>{item}</li>
+                    ))}
+                  </ul>
+                ) : (
+                  <p>{value}</p>
+                )}
+              </div>
+            ))}
         </div>
       </div>
     );
