4b916f9fcec880522250dbaae5b721163bf0b29e
Julia Kim <juliakimseohyeon@gmail.com>
2024-06-19 14:35:01 -0400

    Fix error where ProjectDetail was trying to load before all data was present

--- src/pages/ProjectDetail/ProjectDetail.jsx
+++ src/pages/ProjectDetail/ProjectDetail.jsx
@@ -24,39 +24,48 @@ export default function ProjectDetail() {
     getOneProject();
   }, [id]);
 
-  return (
-    <div className="container">
-      <div>
-        <img src={`${import.meta.env.VITE_API_URL}${selectedProject.image}`} />
-        <div className="project-hero">
-          <div className="project-hero__links">
-            <h1>{selectedProject.project_name}</h1>
-            <p>{selectedProject.url}</p>
-            <p>{selectedProject.github_frontend}</p>
-          </div>
-          <div className="project-hero__tags">
-            {selectedProject.tags.map((tag) => (
-              <p>tag</p>
-            ))}
-          </div>
-        </div>
-        <div className="project-description">
-          {Object.entries(selectedProject.description).map(([key, value]) => (
-            <div key={key}>
-              <h2>{key}</h2>
-              {Array.isArray(value) ? (
-                <ul>
-                  {value.map((item, idx) => (
-                    <li key={idx}>{item}</li>
-                  ))}
-                </ul>
-              ) : (
-                <p>{value}</p>
-              )}
+  if (!selectedProject) {
+    return <h1>Loading project details...</h1>;
+  } else {
+    console.log("Selected project: ", selectedProject);
+    return (
+      <div className="container">
+        <div>
+          <img
+            src={`${import.meta.env.VITE_API_URL}${selectedProject.image}`}
+          />
+          <div className="project-hero">
+            <div className="project-hero__links">
+              <h1>{selectedProject.project_name}</h1>
+              <p>{selectedProject.url}</p>
+              <p>{selectedProject.github_frontend}</p>
             </div>
-          ))}
+            <div className="project-hero__tags">
+              {selectedProject.tags &&
+                selectedProject.tags.map((tag, idx) => <p key={idx}>tag</p>)}
+            </div>
+          </div>
+          <div className="project-description">
+            {selectedProject.description &&
+              Object.entries(selectedProject.description).map(
+                ([key, value]) => (
+                  <div key={key}>
+                    <h2>{key}</h2>
+                    {Array.isArray(value) ? (
+                      <ul>
+                        {value.map((item, idx) => (
+                          <li key={idx}>{item}</li>
+                        ))}
+                      </ul>
+                    ) : (
+                      <p>{value}</p>
+                    )}
+                  </div>
+                )
+              )}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
