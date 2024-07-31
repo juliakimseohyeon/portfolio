711d7bbfdc920a526336a0b312a239de21fcc4be
Julia Kim <juliakimseohyeon@gmail.com>
2024-07-31 15:20:36 -0700

    Edit list styles and add styles to Project Data

--- src/data/ProjectData.jsx
+++ src/data/ProjectData.jsx
@@ -14,7 +14,7 @@ export const projects = [
     project_name: "Kong",
     image: imgKong,
     intro_text:
-      "Get ready to fall in love with nature! Kong is a gamified plant identification app powered by OpenAI and StabilityAI which aims to foster a love for nature in a fun and artful way.",
+      "Get ready to fall in love with nature! Kong is a gamified plant identification app powered by OpenAI and StabilityAI which aims to foster a love for nature in a fun and artistic way.",
     url: "https://kong-app.netlify.app/",
     github_frontend: "https://github.com/juliakimseohyeon/kong-app",
     github_backend: "https://github.com/juliakimseohyeon/kong-app-api",
@@ -30,12 +30,12 @@ export const projects = [
       what: {
         image: imgKongWhat,
         subtitle: "What Is Kong?",
-        text: `<p>Kong helps foster the love for nature by gamifying plant identification. When user uploads or takes a photo of a plant, MyPlantnet identifies it, OpenAI API provides information about the plant, and StabilityAI API generates a Pokémon-like illustration. The identified plant's illustration and information are then added to the user's collection.</p>`,
+        text: `<p>Kong is a <b>gamified plant identification app powered by OpenAI and StabilityAI</b> which aims to foster a love towards nature in a fun and artistic way. When user uploads or takes a photo of a plant, the app identifies the plant and generates a Pokémon-like illustration. The content is then added to the user's collection.</p>`,
       },
       why: {
         image: imgKongWhy,
         subtitle: "Why is Kong necessary?",
-        text: `<p>According to the World Economic Forum, almost 8 out of 10 children do not recognize common plants like an oak leaf or a bluebell. In a world where climate crisis is worse than ever, it is essential that we don't forget about the very basics: fostering a culture where we love and appreciate nature and have empathy towards the environment.</p>`,
+        text: `<p>According to the World Economic Forum, <b>almost 8 out of 10 children do not recognize common plants</b> like an oak leaf or a bluebell. In a world where climate crisis is worse than ever, it is essential that we don't forget about the very basics: fostering a culture where we love and appreciate nature and have empathy towards the environment.</p>`,
       },
       tech_stack: {
         image: imgKongTech,
@@ -97,7 +97,7 @@ export const projects = [
     description: {
       who: {
         image: imgBubblefishWho,
-        subtitle: "Who is Bubblefish UX Design",
+        subtitle: "Who is Bubblefish UX Design?",
         text: `<p>Bubblefish UX Design is a Brighton-based UX consultancy. They have provided holistic UX design services to major UK companies such as 'Chambers and Partners', 'HMRC', 'Public Health Wales', and 'Tristar Worldwide'.</p>`,
       },
       why: {
